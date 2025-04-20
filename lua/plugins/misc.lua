878701d69a577712b53e59dba143c9417694cfcb
lunarmelon <lunarmelon@tuta.io>
2025-04-20 10:31:09 -0600

    replaced npm with pnpm in live-server.nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -48,7 +48,7 @@ return {
 	},
 	{
 		"barrett-ruth/live-server.nvim",
-		build = "npm add -g live-server",
+		build = "pnpm add -g live-server",
 		cmd = { "LiveServerStart", "LiveServerStop" },
 		config = true,
 	},
