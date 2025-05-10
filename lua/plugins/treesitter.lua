120ebcc2032f99c1f7312ca83da459862c3b2f63
lunarmelon <lunarmelon@tuta.io>
2025-05-09 20:37:23 -0600

    chore: add nvim-treesitter-context (sticky scroll)

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -44,7 +44,12 @@ return { -- Highlight, edit, and navigate code
 	--    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
 	--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	dependencies = {
-		"OXY2DEV/markview.nvim",
-		lazy = false,
+		{
+			"OXY2DEV/markview.nvim",
+			ft = { "markdown" },
+		},
+		{
+			"nvim-treesitter/nvim-treesitter-context",
+		},
 	},
 }
