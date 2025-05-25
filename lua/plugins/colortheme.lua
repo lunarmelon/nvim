d8ef92704793a4f7889663813daaf17e5c26253e
lunarmelon <lunarmelon@tuta.io>
2025-05-25 15:37:41 -0600

    chore(colortheme): change theme to tokyonight

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,8 +1,10 @@
 return {
-	"catppuccin/nvim",
+	--"catppuccin/nvim",
+	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme catppuccin-mocha")
+		--vim.cmd("colorscheme catppuccin-mocha")
+		vim.cmd("colorscheme tokyonight-night")
 	end,
 }
