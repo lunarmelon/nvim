1a73a53a161d766803b5372d998def0f5e837da2
lunarmelon <lunarmelon@tuta.io>
2025-05-25 15:52:06 -0600

    chore(ibl): reorder color progression

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,10 +1,10 @@
 local config = function()
 	local highlight = {
 		"RainbowRed",
-		"RainbowYellow",
-		"RainbowBlue",
 		"RainbowOrange",
+		"RainbowYellow",
 		"RainbowGreen",
+		"RainbowBlue",
 		"RainbowViolet",
 		"RainbowCyan",
 	}
@@ -14,10 +14,10 @@ local config = function()
 	-- every time the colorscheme changes
 	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
 		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
-		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
-		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
 		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
 		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
 		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
 		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 	end)
